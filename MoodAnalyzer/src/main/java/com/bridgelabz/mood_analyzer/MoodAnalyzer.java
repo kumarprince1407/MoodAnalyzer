aee6b0635039800c2e82499771f2c4657b8cd350
Prince Kumar <kumarprince1407@gmail.com>
2023-07-16 23:09:11 +0530

    Added Use case 1

--- MoodAnalyzer/src/main/java/com/bridgelabz/mood_analyzer/MoodAnalyzer.java
+++ MoodAnalyzer/src/main/java/com/bridgelabz/mood_analyzer/MoodAnalyzer.java
@@ -1,7 +1,15 @@
 package com.bridgelabz.mood_analyzer;
 
 public class MoodAnalyzer {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Mood Analyzer program.");
+    public String alalyseMood(String message){
+    if(message.contains("Sad")){
+        return "SAD";
+    }else{
+        return "HAPPY";
     }
+    }
+//    public static void main(String[] args) {
+//        MoodAnalyzer moodAnalyzer1 = new MoodAnalyzer();
+//        moodAnalyzer1.alalyseMood();
+//    }
 }
--- MoodAnalyzer/src/test/java/MoodAnalyzerTest.java
+++ MoodAnalyzer/src/test/java/MoodAnalyzerTest.java
@@ -1,10 +1,20 @@
 import com.bridgelabz.mood_analyzer.MoodAnalyzer;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class MoodAnalyzerTest {
     @Test
-    public void testResult(){
+    public void SadMood(){
+    MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+    String mood = moodAnalyzer.alalyseMood("I am in Sad mood.");
+        Assert.assertEquals("SAD",mood);
+    }
 
+    @Test
+    public void HappyMood(){
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+        String mood = moodAnalyzer.alalyseMood("I am in Any mood.");
+        Assert.assertEquals("HAPPY",mood);
     }
 
 }
